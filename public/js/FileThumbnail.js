a347efb9dd28c0b8a8bf0d92328558e52c16087b
Dallin Cox <dallincox@gmail.com>
2015-12-10 15:44:19 -0700

    For now, when you click on the thumbnail, it'll just load up the actual picture.

--- public/js/FileThumbnail.js
+++ public/js/FileThumbnail.js
@@ -2,18 +2,19 @@
 
 var React  = require('react');
 
-var FileThumbnail = React.createClass({
-	render: function() {
-		return (
-			<div style={{float: 'left'}} className='col-md-3 hoverable'>
-				<div style={{position: 'relative'}}>
-					<h4 style={{textAlign: 'center'}}>{this.props.data.title}</h4>
-					<div style={{textAlign: 'center', fontStyle: 'italic'}}>Composer: {this.props.data.composer}</div>
-					<img src={'media/'+this.props.data.thumbnail_file} className="thumbnail_pic" />
-				</div>
-			</div>
+var FileThumbnail = React.createClass({displayName: "FileThumbnail",
+		render: function() {
+			return (
+                React.createElement("a", {href:this.props.data.thumbnail_file},
+				React.createElement("div", {style: {float: 'left'}, className: "col-md-3 hoverable"}, 
+					React.createElement("div", {style: {position: 'relative'}}, 
+						React.createElement("h4", {style: {textAlign: 'center'}}, "Title: ", this.props.data.title), 
+						React.createElement("div", {style: {textAlign: 'center', fontStyle: 'italic'}}, "Composer: ", this.props.data.composer),
+						React.createElement("img", {src: this.props.data.thumbnail_file, className: "thumbnail_pic"})
+					)
+				)
+                )
 			);
-	}
-});
-
+		}
+	});
 module.exports = FileThumbnail;
