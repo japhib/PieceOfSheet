6128c5a6512933a9f487f084c9274f6b091039ea
Dallin Cox <dallincox@gmail.com>
2015-12-10 20:07:13 -0700

    Added View.js

---
+++ public/js/View.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var React  = require('react');
+
+var CommentBox = require('./CommentBox');
+var BrowseHeader = require('./BrowseHeader');
+
+var View = React.createClass({
+    componentWillMount:function() {
+        this.data = JSON.parse(localStorage.getItem("data"))
+        localStorage.removeItem("data")
+        console.log(this.data)
+    },
+    render: function() {
+        return(
+            <div className="col-xs-12 col-sm-6 col-lg-8 musicInfo">
+                <div className='musicData'>
+                <h1>Title: {this.data.title}</h1>
+                <h3>Composer: {this.data.composer}</h3>
+                    <img src={this.data.filename} />
+                </div>
+            </div>
+        )
+    }
+});
+
+module.exports = View;
