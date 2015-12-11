c61144298cfafe4c5a0d4e0e41fd30d9ebcf637b
Dallin Cox <dallincox@gmail.com>
2015-12-10 21:09:57 -0700

    View image is good size, centered

--- public/js/View.js
+++ public/js/View.js
@@ -13,12 +13,10 @@ var View = React.createClass({
     },
     render: function() {
         return(
-            <div className="col-xs-12 col-sm-6 col-lg-8 musicInfo">
-                <div className='musicData'>
+            <div className="welcome">
                 <h1>Title: {this.data.title}</h1>
                 <h3>Composer: {this.data.composer}</h3>
-                    <iframe id="iframe" src={"media/" + this.data.filename}></iframe>
-                </div>
+                    <iframe src={"media/" + this.data.filename} width="650" height="700"></iframe>
             </div>
         )
     }
